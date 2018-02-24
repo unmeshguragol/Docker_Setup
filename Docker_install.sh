f20fd49150d31f654f629e5271ab2f8db9353374
unmeshguragol <unmeshguragol@gmail.com>
2018-02-24 09:43:21 +0530

    Update Docker_install.sh

--- Docker_install.sh
+++ Docker_install.sh
@@ -1,6 +1,14 @@
-sudo su
-apt-get update
-apt-get install \ apt-transport-https \ ca-certificates \ curl \ gnupg2 \ software-properties-common
-curl -fsSL https://download.docker.com/linux/$(. /etc/s-release; echo "$ID")/gpg | sudo apt-key add - add-apt-repository \ "deb [arch=amd64] https://download.docker.com/linux/$(. /etc/os-release; echo "$ID") \ $(lsb_release -cs) \ stable"
-apt-get update
-apt-get install docker-ce
+sudo apt-get update
+sudo apt-get install \
+     apt-transport-https \
+     ca-certificates \
+     curl \
+     gnupg2 \
+     software-properties-common
+curl -fsSL https://download.docker.com/linux/$(. /etc/os-release; echo "$ID")/gpg | sudo apt-key add -
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/$(. /etc/os-release; echo "$ID") \
+   $(lsb_release -cs) \
+   stable"
+sudo apt-get update
+sudo apt-get install docker-ce
